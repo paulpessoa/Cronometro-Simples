c9848bf3b38441e90a14a60d04faf8cec1429015
Paul Pessoa <paulmspessoa@gmail.com>
2021-05-10 03:33:17 -0300

    adição milisegundos

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
  let hora = 0;
  let minuto = 0;
  let segundo = 0;
+ let milisegundo = 0;
 
-let tempo = 1000; //1 segundo sao  1000 milésimos.
+let tempo = 10; //1 segundo sao  1000 milésimos.
 let cronometro;
 
  function iniciar (){
@@ -22,25 +23,34 @@ let cronometro;
     hora = 0;
     minuto = 0;
     segundo = 0;
+    milisegundo = 0;
 
-    document.getElementById('counter').innerHTML = '00:00:00';
+    document.getElementById('counter').innerHTML = '00:00:00:00';
 }
 
  function timer() {
 
-    segundo++;
-    
-    if (segundo == 60){
-        segundo = 0;
-        minuto++;
+    milisegundo++;
+    if (milisegundo == 100){
+        milisegundo = 0;
+        segundo++;
 
-        if (minuto == 60){
-            minuto = 0;
-            hora++;
+        if (segundo == 60){
+            segundo = 0;
+            minuto++;
+
+            if (minuto == 60){
+                minuto = 0;
+                hora++;
+            }
         }
     }
-
-     let formato = (hora < 10 ? '0' + hora : hora) + ":" + (minuto < 10 ? '0'+ minuto : minuto) + ":" + (segundo < 10 ? '0' + segundo : segundo); 
+     let formato = 
+     (hora < 10 ? '0' + hora : hora) + ":" + 
+     (minuto < 10 ? '0'+ minuto : minuto) + ":" + 
+     (segundo < 10 ? '0' + segundo : segundo) + ":" + 
+     (milisegundo < 10 ? '0' + milisegundo : milisegundo); 
+          
      document.getElementById('counter').innerHTML = formato;
 
      return formato;
